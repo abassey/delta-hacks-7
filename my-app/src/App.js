48fe1bf210d0048f71e03176c854143b76c92241
abassey <anemmeabasi1@gmail.com>
2021-03-06 17:52:38 -0500

    base home added, bootstrap not showing on localhost

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#">Matches</Nav.Link>
+              <Nav.Link href="#">Chats</Nav.Link>
+              <Nav.Link href="#">Events</Nav.Link>
+              <Nav.Link href="#">Account</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <section data-section="home-header">
+          <Jumbotron fluid>
+            <Container>
+              <h1>Welcome Home, John!</h1>
+              <p>
+              Here's what's happened since the last time you were here...
+              </p>
+            </Container>
+          </Jumbotron>
+        </section>
+
+        <section data-section="interests">
+          <div>
+            <h3>Interests</h3>
+
+
+          </div>
+        </section>
+
+        <section data-section="home-events">
+        <CardDeck>
+          <Card>
+            <Card.Body>
+              <Card.Title>Missed Messages</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Upcoming Events</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Hashtags You Follow</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+        </section>
+      </div>
+    )
+  }
 }
-
-export default App;
