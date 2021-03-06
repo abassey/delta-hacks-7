d883b8ce460babfd7cedd7a1f4606d3bd9950dfb
impedaka <qinfengyu123@gmail.com>
2021-03-06 13:49:21 -0500

    more features!!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
 
+import ChatFeed from './components/ChatFeed';
+
 const App = () => {
     return (
         <ChatEngine 
@@ -9,8 +11,9 @@ const App = () => {
             projectID="91ab2466-2b27-4441-97ba-23aa187737fc"
             userName="impedaka"
             userSecret="maomao24"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
-    )
+    );
 }
 
 export default App;
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,20 @@
+const MyMessage = ({ message }) => {
+    //submit image feature
+    if(message?.attachments?.length > 0) {
+        return (
+            <img 
+                src={message.attachments[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{ float: 'right'}}            
+            />
+        )
+    } //otherwise submit message
+    return (
+        <div className="message" style={{float:'right', marginRight: '18px', color:'white', backgroundColor: 'rgb(22,188,237,1)' }}>
+            {message.text}
+        </div>
+    );
+}
+
+export default MyMessage;
