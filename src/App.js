c0c0c38a3ce5c2a719449da24ffd94ac9287a504
impedaka <qinfengyu123@gmail.com>
2021-03-06 15:03:59 -0500

    more adjustments

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import './App.css';
 import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
-const App = () => {
-    if(!localStorage.getItem('username')) return <LoginForm />
-    return (
-        <ChatEngine 
-            height="100vh"
-            projectID="91ab2466-2b27-4441-97ba-23aa187737fc"
-            userName="impedaka"
-            userSecret="123"
-            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-        />
-    );
-}
+const projectID = '91ab2466-2b27-4441-97ba-23aa187737fc'
 
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+  
+    return (
+      <ChatEngine
+        height="100vh"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      />
+    );
+  };
 export default App;
